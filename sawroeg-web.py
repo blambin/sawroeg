1077fb6d6c2adf8fe65690ad24275b50f47d24fd
blambin <blambin@blambin.org>
2013-09-11 13:09:08 +0400

    sync

--- sawroeg-web.py
+++ sawroeg-web.py
@@ -92,7 +92,7 @@ class ApiHandler(tornado.web.RequestHandler):
 
 if __name__ == "__main__":
     tornado.options.define("debug", default=False, help="enabling debugging features", type=bool)
-    tornado.options.define("port", default=7777, help="run on the given port", type=int)
+    tornado.options.define("port", default=7778, help="run on the given port", type=int)
     tornado.options.parse_command_line()
     app_settings = {
         'gzip': True,
